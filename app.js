67d9e48ca902381cd9dd6cc120353c1ee152f51e
Virginia Moore <vmoore0607@yahoo.com>
2018-12-06 10:49:57 -0500

    added new file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('./instantHello');
 var goodbye = require('./talk/goodbye');
 var talk = require('./talk');
+var question = require('./talk/question');
+
 
 talk.intro(); 
 talk.hello("Virginia");
 
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
 
 goodbye();
